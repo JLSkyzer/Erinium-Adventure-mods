539b2c15ac4893173ae5e2b38d247d9fcbeb2bb8
JLSkyzer <killiandasneves4@gmail.com>
2024-01-17 23:20:39 +0100

    okkk it's heeere

--- Hunter/src/main/java/fr/eriniumgroup/eriniumadventure/base/procedures/RightClickBlockProcedure.java
+++ Hunter/src/main/java/fr/eriniumgroup/eriniumadventure/base/procedures/RightClickBlockProcedure.java
@@ -1,19 +1,10 @@
 package fr.eriniumgroup.eriniumadventure.base.procedures;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.ChunkPos;
+import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-
-import net.minecraft.world.level.chunk.LevelChunk;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.network.chat.Component;
-import net.minecraft.client.Minecraft;
 
 import javax.annotation.Nullable;
 
@@ -23,25 +14,43 @@ public class RightClickBlockProcedure {
 	public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
 		if (event.getHand() != event.getEntity().getUsedItemHand())
 			return;
-		execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getZ(), event.getEntity());
+		execute(event);
 	}
 
-	public static void execute(LevelAccessor world, double x, double z, Entity entity) {
-		execute(null, world, x, z, entity);
+	public static void execute() {
+		execute(null);
 	}
 
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double z, Entity entity) {
-		if (entity == null)
-			return;
-		if (entity instanceof Player _player && !_player.level().isClientSide())
-			_player.displayClientMessage(Component.literal(new Object() {
-				private String getChunkX(int x, int z) {
-					BlockPos blockpos = Minecraft.getInstance().getCameraEntity().blockPosition();
-					ChunkPos chunkpos = new ChunkPos(new BlockPos(40, 0, 23));
-					ChunkPos chunkpos2 = new ChunkPos(new BlockPos(-13, 0, -30));
+	private static void execute(@Nullable Event event) {
+		if (new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs"))).isDirectory() && new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs"))).exists()) {
+			// Récupérer la liste des fichiers et sous-dossiers dans le dossier
+			java.io.File[] fichiers = new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs"))).listFiles();
+			// Parcourir la liste et supprimer chaque fichier/dossier
+			if (fichiers != null) {
+				for (java.io.File fichier : fichiers) {
+					fichier.delete();
 				}
-			}.getChunkX((int) x, (int) z)), false);
+			}
+			if (new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs"))).listFiles() != null) {
+				new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs"))).delete();
+			} else {
+				System.out.println("Unable to delete the folder, it seems that a file was recreated after deleting all the files");
+			}
+		}
+		if (new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs2"))).isDirectory() && new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs2"))).exists()) {
+			// Récupérer la liste des fichiers et sous-dossiers dans le dossier
+			java.io.File[] fichiers = new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs2"))).listFiles();
+			// Parcourir la liste et supprimer chaque fichier/dossier
+			if (fichiers != null) {
+				for (java.io.File fichier : fichiers) {
+					fichier.delete();
+				}
+			}
+			if (new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs2"))).listFiles() != null) {
+				new java.io.File(new String((FMLPaths.GAMEDIR.get().toString() + "/defaultconfigs2"))).delete();
+			} else {
+				System.out.println("Unable to delete the folder, it seems that a file was recreated after deleting all the files");
+			}
+		}
 	}
-
-
 }
